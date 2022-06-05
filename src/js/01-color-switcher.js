c58f2e40ac3e560cf7dbc3d4bc57ec006cb56e16
Yuliia Kovalchuk <iuliia1702@gmail.com>
2022-06-05 13:42:22 +0300

    feature/task-01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const refs = {
+  bodyEl: document.querySelector('body'),
+  startBtn: document.querySelector('[data-start]'),
+  stopBtn: document.querySelector('[data-stop]'),
+};
+let timerId = null;
+
+refs.startBtn.addEventListener('click', onClickStartBtnChangeBodyColor);
+refs.stopBtn.addEventListener('click', onClickStopBtnStopChangeBodyColor);
+
+function onClickStartBtnChangeBodyColor() {
+  timerId = setInterval(() => {
+    refs.bodyEl.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  if (timerId !== null) {
+    refs.startBtn.setAttribute('disabled', true);
+    refs.stopBtn.removeAttribute('disabled');
+  }
+}
+
+function onClickStopBtnStopChangeBodyColor() {
+  clearInterval(timerId);
+  refs.startBtn.removeAttribute('disabled');
+  refs.stopBtn.setAttribute('disabled', true);
+}
