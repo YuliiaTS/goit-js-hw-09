62788e29772fbc01d134d1e5860c8e5f276ec888
Yuliia Kovalchuk <iuliia1702@gmail.com>
2022-06-09 21:46:10 +0300

    bugfix/task-02

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,33 +1,33 @@
 import '../css/common.css';
-import flatpickr from "flatpickr";
-import "flatpickr/dist/flatpickr.min.css";
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
 import Notiflix from 'notiflix';
 
 const refs = {
-    startBtn: document.querySelector('[data-start]'),
-    daysEl: document.querySelector('[data-days]'),
-    hoursEl: document.querySelector('[data-hours]'),
-    minutesEl: document.querySelector('[data-minutes]'),
-    secondsEl: document.querySelector('[data-seconds]'),
-}
+  startBtn: document.querySelector('[data-start]'),
+  daysEl: document.querySelector('[data-days]'),
+  hoursEl: document.querySelector('[data-hours]'),
+  minutesEl: document.querySelector('[data-minutes]'),
+  secondsEl: document.querySelector('[data-seconds]'),
+};
 let selectedTime = null;
 
 const options = {
-    enableTime: true,
-    time_24hr: true,
-    defaultDate: new Date(),
-    minuteIncrement: 1,
-    onClose(selectedDates) {
-      const presentTime = Date.now();
-      selectedTime = selectedDates[0];
-      if (presentTime > selectedTime) {
-        Notiflix.Notify.failure(`Please choose a date in the future`);
-        refs.startBtn.setAttribute('disabled', true);
-        return;
-      } else {
-        refs.startBtn.removeAttribute('disabled');
-      }
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    const presentTime = Date.now();
+    selectedTime = selectedDates[0];
+    if (presentTime > selectedTime) {
+      Notiflix.Notify.failure(`Please choose a date in the future`);
+      refs.startBtn.setAttribute('disabled', true);
+      return;
+    } else {
+      refs.startBtn.removeAttribute('disabled');
     }
+  },
 };
 flatpickr('#datetime-picker', options);
 
@@ -49,6 +49,15 @@ const timer = {
       const newTime = convertMs(deltaTime);
       updateClockFace(newTime);
 
+      if (deltaTime <= 0) {
+        clearInterval(this.intervalId);
+        updateClockFace({
+          days: '00',
+          hours: '00',
+          minutes: '00',
+          seconds: '00',
+        });
+      }
     }, 1000);
   },
 };
@@ -58,17 +67,17 @@ function pad(value) {
 }
 
 function convertMs(ms) {
-    const second = 1000;
-    const minute = second * 60;
-    const hour = minute * 60;
-    const day = hour * 24;
-  
-    const days = pad(Math.floor(ms / day));
-    const hours = pad(Math.floor((ms % day) / hour));
-    const minutes = pad(Math.floor(((ms % day) % hour) / minute));
-    const seconds = pad(Math.floor((((ms % day) % hour) % minute) / second));
-  
-    return { days, hours, minutes, seconds };
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  const days = pad(Math.floor(ms / day));
+  const hours = pad(Math.floor((ms % day) / hour));
+  const minutes = pad(Math.floor(((ms % day) % hour) / minute));
+  const seconds = pad(Math.floor((((ms % day) % hour) % minute) / second));
+
+  return { days, hours, minutes, seconds };
 }
 
 function updateClockFace({ days, hours, minutes, seconds }) {
